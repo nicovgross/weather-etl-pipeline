28e058a830f26a00dfd5bd301b020ca1b9826695
nicovgross <nicholasvallegross@gmail.com>
2026-07-28 20:03:45 -0300

    Refactor main.py

--- etl/src/main.py
+++ etl/src/main.py
@@ -8,80 +8,84 @@ import json
 import logging
 import sys
 
-load_dotenv()
+def run_pipeline():
+    load_dotenv()
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_DEFAULT_REGION")
-)
-
-BUCKET = os.getenv("AWS_BUCKET_NAME")
-
-def upload_file_s3(local_path):
-    s3_key = os.path.relpath(local_path, "data")
-    s3.upload_file(
-        Filename=local_path,
-        Bucket=BUCKET,
-        Key=s3_key
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_DEFAULT_REGION")
     )
-    logging.info(f"Uploaded {s3_key} to s3")
 
-if len(sys.argv) == 1:
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-elif len(sys.argv) > 1 and sys.argv[1] == "no-log": 
-    no_log = True
-    pass
-else: 
-    print("Argument invalid")
-    sys.exit(1)
+    BUCKET = os.getenv("AWS_BUCKET_NAME")
+
+    def upload_file_s3(local_path):
+        s3_key = os.path.relpath(local_path, "data")
+        s3.upload_file(
+            Filename=local_path,
+            Bucket=BUCKET,
+            Key=s3_key
+        )
+        logging.info(f"Uploaded {s3_key} to s3")
+
+    if len(sys.argv) == 1:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s"
+        )
+    elif len(sys.argv) > 1 and sys.argv[1] == "no-log": 
+        no_log = True
+        pass
+    else: 
+        print("Argument invalid")
+        sys.exit(1)
 
 
-with open("config/cities.json", "r") as f:
-    cities = json.load(f)
+    with open("config/cities.json", "r") as f:
+        cities = json.load(f)
 
-logging.info("Starting pipeline")
+    logging.info("Starting pipeline")
 
-total_records = 0
-for city in cities:
+    total_records = 0
+    for city in cities:
 
-    logging.info(f"Processing city: {city['city_name']}")
+        logging.info(f"Processing city: {city['city_name']}")
 
-    try:
-        params = { #Define parameters
-            "city_name": city["city_name"],
-            "latitude": city["latitude"],
-            "longitude": city["longitude"],
-            "hourly": ["temperature_2m", "apparent_temperature", "relative_humidity_2m", "precipitation_probability",
-                        "precipitation", "windspeed_10m", "winddirection_10m", "weathercode", "cloudcover"],
-            "timezone": city["timezone"], # Local timezone based on your location
-            "current_weather": True
-        }
+        try:
+            params = { #Define parameters
+                "city_name": city["city_name"],
+                "latitude": city["latitude"],
+                "longitude": city["longitude"],
+                "hourly": ["temperature_2m", "apparent_temperature", "relative_humidity_2m", "precipitation_probability",
+                            "precipitation", "windspeed_10m", "winddirection_10m", "weathercode", "cloudcover"],
+                "timezone": city["timezone"], # Local timezone based on your location
+                "current_weather": True
+            }
 
-        # EXTRACTION
-        logging.info("  Extracting data from API...")
-        raw_file_path, num_records = extract_data(params)
-        upload_file_s3(raw_file_path)
-        logging.info(f"  {num_records} records extracted")
-        total_records += num_records
+            # EXTRACTION
+            logging.info("  Extracting data from API...")
+            raw_file_path, num_records = extract_data(params)
+            upload_file_s3(raw_file_path)
+            logging.info(f"  {num_records} records extracted")
+            total_records += num_records
 
-        #TRANSFORMATION
-        logging.info("  Transforming...")
-        hourly_paths, daily_paths = transform_data(raw_file_path, city["city_name"])
+            #TRANSFORMATION
+            logging.info("  Transforming...")
+            hourly_paths, daily_paths = transform_data(raw_file_path, city["city_name"])
 
-        #LOADING
-        logging.info("  Loading into database...")
-        for hourly, daily in zip(hourly_paths, daily_paths):
-            load_data(hourly, daily)
-            upload_file_s3(hourly)
-            upload_file_s3(daily)
+            #LOADING
+            logging.info("  Loading into database...")
+            for hourly, daily in zip(hourly_paths, daily_paths):
+                load_data(hourly, daily)
+                upload_file_s3(hourly)
+                upload_file_s3(daily)
 
-    except Exception:
-        logging.exception(f"Error processing city {city['city_name']}")
+        except Exception:
+            logging.exception(f"Error processing city {city['city_name']}")
 
-logging.info(f"Total of {total_records} records extracted")
-logging.info("Pipeline finished")
+    logging.info(f"Total of {total_records} records extracted")
+    logging.info("Pipeline finished")
+
+if __name__ == "__main__":
+    run_pipeline()
